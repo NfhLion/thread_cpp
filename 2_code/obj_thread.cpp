bcfe0b0010d6e87be93351c3f241e2c414c8d4ac
fenghai.ning <fenghai.ning@megatronix.co>
2022-08-08 13:53:24 +0800

    [CHG]: add RAII thread_guard

--- 2_code/obj_thread.cpp
+++ 2_code/obj_thread.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <unistd.h>
 
 // void doSomeThing() {
 //     std::cout << "run doSomeThing()\n" << std::endl;
@@ -8,15 +9,27 @@
 class BackgroundTask {
 public:
     void operator()() const {
+        sleep(1);
         std::cout << "run doSomeThing()\n" << std::endl;
     }
 };
 
-int main() {
+void testThread() {
     BackgroundTask task;
     std::thread t(task);
     t.join();
+}
+
+int main() {
+    BackgroundTask task;
+    std::thread t(task);
+    // std::cout << "this is main thread" << std::endl;
+    // t.join();
+    t.detach();
     // std::thread t(BackgroundTask()); // 不能这么写，原因是编译器会把t当做是一个函数
+    //testThread();
+    std::cout << "this is main thread" << std::endl;
+    while(1);
 
     return 0;
 }
--- 2_code/scoped_thread.cpp
+++ 2_code/scoped_thread.cpp
@@ -1,6 +1,27 @@
 #include <thread>
 #include <iostream>
 
+// RAII
+class thread_guard {
+public:
+    explicit thread_guard(std::thread& t)
+        : t_(t)
+    {
+
+    }
+    ~thread_guard() {
+        if (t_.joinable()) {
+            t_.join();
+        }
+    }
+
+    thread_guard(const thread_guard& tg) = delete;
+    thread_guard& operator=(const thread_guard& tg) = delete;
+
+private:
+    std::thread& t_;
+};
+
 class scoped_thread {
 public:
     explicit scoped_thread(std::thread t)
