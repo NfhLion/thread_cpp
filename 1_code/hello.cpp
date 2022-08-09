01ef0ef1a97a4d725c33c41888b406ff603fa81e
fenghai.ning <fenghai.ning@megatronix.co>
2022-08-09 20:36:13 +0800

    [ADD]: add joining_thread.cpp

--- 1_code/hello.cpp
+++ 1_code/hello.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <thread>
+#include <unistd.h>
+#include <string>
+using std::string;
 
-void threadFunc() {
-    std::cout << "hello Concurrent world\n" << std::endl;
+void threadFunc(string str) {
+    while (true) {
+        std::cout << "threadFunc thread id = " << std::this_thread::get_id() << "----------" << str << std::endl;
+        sleep(1);
+    }
+}
+
+void threadFunc2(string str) {
+    while (true) {
+        std::cout << "threadFunc2 thread id = " << std::this_thread::get_id() << "----------" << str << std::endl;
+        sleep(1);
+    }
 }
 
 int main() {
-    std::thread t(threadFunc);
+    std::thread t(threadFunc, "t");
+    std::thread t2(threadFunc2, "t2");
     t.join();
+    t2.join();
 
+    t = std::move(t2);
     return 0;
 }
--- 2_code/ref_thread.cpp
+++ 2_code/ref_thread.cpp
@@ -4,7 +4,7 @@
 
 using std::string;
 
-void func(int i, const string& str) {
+void func(int i, const string&& str) {  // 这里只能是右值、值传递，或者const引用，原因见《C++并发编程实战》P23页
     sleep(1);
     // str = "abcde";
 }
